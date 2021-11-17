0b2be60c483af18afab40784fd71796d705bafce
Budi Darmawan <budi.darmawan@buaya.dev>
2021-11-18 06:46:49 +0700

    add extension file

---
+++ app/src/main/java/com/bddrmwan/cocktailcatalogue/main/extension/ActivityExt.kt
@@ -0,0 +1,12 @@
+package com.bddrmwan.cocktailcatalogue.main.extension
+
+import android.app.Activity
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+
+fun Activity.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun Activity.toastLong(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+fun Fragment.toast(message: String) = Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+fun Fragment.toastLong(message: String) = Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
