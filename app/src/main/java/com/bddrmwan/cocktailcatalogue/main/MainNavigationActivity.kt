421d63b01f9c2bd47a540d7a04b45eda9c39b8b9
Budi Darmawan <budi.darmawan@buaya.dev>
2021-11-19 21:50:09 +0700

    add the function of pressing back twice to exit the app

--- app/src/main/java/com/bddrmwan/cocktailcatalogue/main/MainNavigationActivity.kt
+++ app/src/main/java/com/bddrmwan/cocktailcatalogue/main/MainNavigationActivity.kt
@@ -2,16 +2,31 @@ package com.bddrmwan.cocktailcatalogue.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bddrmwan.cocktailcatalogue.R
 import com.bddrmwan.cocktailcatalogue.databinding.ActivityMainNavigationBinding
+import com.bddrmwan.cocktailcatalogue.main.extensions.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainNavigationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainNavigationBinding
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        toast(getString(R.string.press_back_again))
+        Handler(Looper.getMainLooper()).postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
     }
 }
